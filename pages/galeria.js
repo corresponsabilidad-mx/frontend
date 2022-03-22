cdf734d478fff3780d34dfcf436d0aa0fd1697af
Mar <marina.falcon@gmail.com>
2022-03-22 12:49:26 +0100

    Add pages

---
+++ pages/galeria.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function Galeria() {
+	return (
+	  <>
+		<h1>GALERIA</h1> 
+	  </>    
+	  
+	)
+}
---
+++ pages/proyectos.jsx
@@ -0,0 +1,7 @@
+export default function Proyectos() {
+	return (
+	  <>
+	     <div className="container mt-5">PROYECTOS</div>        
+	  </>
+	)
+}
