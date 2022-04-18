031a2277ec68dbfb85288dabf4175fb8a1fc65e1
silvi-sanchez <silvinans@gmail.com>
2022-04-18 17:18:53 -0300

    changes-styles-proyectos

--- components/Alianzas.js
+++ components/Alianzas.js
@@ -1,24 +1,24 @@
-import Image from "next/image";
+// import ImageViewer from "react-simple-image-viewer";
 
 const Alianzas= () => {
     const imgAlianzas = [
+        "/images/alianzas/alianza17.png",
         "/images/alianzas/alianza1.jpg",
-        "/images/alianzas/alianza2.png",
         "/images/alianzas/alianza3.png",
         "/images/alianzas/alianza4.png",
-        "/images/alianzas/alianza5.jpg",
         "/images/alianzas/alianza6.png",
         "/images/alianzas/alianza7.png",
-        "/images/alianzas/alianza8.png",
+        "/images/alianzas/alianza15.png",
         "/images/alianzas/alianza9.png",
         "/images/alianzas/alianza10.png",
+        "/images/alianzas/alianza2.png",
         "/images/alianzas/alianza11.png",
         "/images/alianzas/alianza12.png",
         "/images/alianzas/alianza13.png",
         "/images/alianzas/alianza14.png",
-        "/images/alianzas/alianza15.png",
+        "/images/alianzas/alianza8.png",
         "/images/alianzas/alianza16.png",
-        "/images/alianzas/alianza17.png",
+        "/images/alianzas/alianza5.jpg",
         "/images/alianzas/alianza18.png",
       ];
 
@@ -26,18 +26,16 @@ const Alianzas= () => {
     <>
         <h4 className="titulo-alianzas">Alianzas estratégicas</h4>
         <div className="div-prin-alianzas">
-            {imgAlianzas.map((imgAlianza, index) => (
-                
+            {imgAlianzas.map((imgAlianza, index) => ( 
+
                 <li className="img-li" key={index}>
-                    <Image
+                    <img
                     className="pb-2 ml-5 mr-5 img-alianzas"
                     src={imgAlianza}
                     alt="alianzas"
-                    width={100}
-                    height={80}
-                    />
-                </li>
-                
+                    height="70"
+                    />       
+                </li>  
             ))} 
         </div>
     </>
