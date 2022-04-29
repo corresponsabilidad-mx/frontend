cd1020bb8e5cb630ed0509a7c22f0b50a7adcd96
MarianaDeleau <marianadeleau@gmail.com>
2022-04-29 12:46:00 -0300

    fix card responsive and image formatt

--- components/Formulario.js
+++ components/Formulario.js
@@ -19,7 +19,7 @@ const Form = () => {
           <div className="contact align-items-start">
             <p className="letra-form">
               <BsTelephoneFill className="footer-icono me-1" />
-              <span>Teléfono</span>
+              <span>Teléfono: (52) 55 5084 0174</span>
             </p>
             <p className="letra-form">
               <BsFillEnvelopeFill className="footer-icono me-1" />
