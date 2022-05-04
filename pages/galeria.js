5ce31501f30a0fbadc8905515a1cc7f8afe17712
Mar <marina.falcon@gmail.com>
2022-05-04 17:02:34 +0200

    Adding style details

--- pages/galeria.js
+++ pages/galeria.js
@@ -296,7 +296,7 @@ export default function Galeria() {
   };
 
   return (
-    <div className="container">
+    <div className="container pt-4 pb-3">
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
           <Accordion.Header>
